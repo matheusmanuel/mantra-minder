4bacd5de9dc0f081033adbea2def1c12172b9ebd
Matheus manuel <matheusmanuel111@gmail.com>
2023-10-17 09:58:36 +0100

    new feature[backend] update mantra ✔✔

--- backend/src/controllers/mantraController.js
+++ backend/src/controllers/mantraController.js
@@ -12,28 +12,61 @@ function insertMantra(req, res) {
     let mantra = req.body;
     let sql = `INSERT INTO Mantras (mantraTitle, mantraText, isActive, playOnStartup, displayTime)
         VALUES (?,?,?,?,?);`;
-    db.run(sql, [
-      mantra.mantraTitle,
-      mantra.mantraText,
-      mantra.isActive,
-      mantra.playOnStartup,
-      mantra.displayTime,
-    ], async function(error){
-        if(error){
-            console.log("erro[sql] na hora de cadastrar um mantra: ",error.message);
-            res.status(500).json({msg: `Erro ao inserir um mantra: ${error.message}`});
-            return false;
-        }else{
-            console.log(`Mantra cadastrado com sucesss: ${this.lastID}`);
-            res.status(200).json({msg: `Mantra cadastrado com sucesso: ${this.lastID}`});
+    db.run(
+      sql,
+      [
+        mantra.mantraTitle,
+        mantra.mantraText,
+        mantra.isActive,
+        mantra.playOnStartup,
+        mantra.displayTime,
+      ],
+      async function (error) {
+        if (error) {
+          console.log(
+            "erro[sql] na hora de cadastrar um mantra: ",
+            error.message
+          );
+          res
+            .status(500)
+            .json({ msg: `Erro ao inserir um mantra: ${error.message}` });
+          return false;
+        } else {
+          console.log(`Mantra cadastrado com sucesss: ${this.lastID}`);
+          res
+            .status(200)
+            .json({ msg: `Mantra cadastrado com sucesso: ${this.lastID}` });
         }
-    });
+      }
+    );
   } catch (error) {
     console.error("Houve um erro ao inserir um mantra: ", error);
     res.status(500).json({ msg: `Erro ao inserir um mantra: ${error}` });
   }
 }
 
-module.exports = {
-    insertMantra,
+function updateMantra(req, res) {
+  try{
+    let mantra = req.body;
+    let sql = `UPDATE Mantras SET mantraTitle = ?, mantraText = ?, isActive = ?, playOnStartup = ?, displayTime = ? WHERE mantraID = ?`;
+
+    db.run(sql, [mantra.mantraTitle, mantra.mantraText, mantra.isActive, mantra.playOnStartup, mantra.displayTime, mantra.mantraID], async function(error){
+      if(error){
+        res.status(500).json({msg: `[sql]Houve um erro ao editar o mantra: ${error.message}`});
+        console.log("[sql]Houve um erro ao editar o mantra cmd: ", error.message);
+        return false;
+      }else{
+        res.status(200).json({msg:`O mantra com id: ${mantra.mantraID} foi editado com sucesso`});
+        console.log(`O mantra com id: ${mantra.mantraID} foi editado com sucesso`);
+      }
+    })
+  }catch(error){
+    console.error("Houve um erro ao editar um mantra: ", error);
+    res.status(500).json({ msg: `Erro ao editar um mantra: ${error}` });
+  }
 }
+
+module.exports = {
+  insertMantra,
+  updateMantra
+};
--- backend/src/routes/mantraRoutes.js
+++ backend/src/routes/mantraRoutes.js
@@ -3,5 +3,8 @@ const mantraRoutes = express.Router();
 const mantraController = require('../controllers/mantraController');
 
 mantraRoutes.post('/mantra/insert', mantraController.insertMantra);
+mantraRoutes.put('/mantra/update', mantraController.updateMantra);
+// mantraRoutes.get('/mantras', )
+// mantraRoutes.delete('/mantra')
 
 module.exports = mantraRoutes;
