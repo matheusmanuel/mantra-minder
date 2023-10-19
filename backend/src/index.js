b9eab2bc3a98c0e0c54c205f1680d140509b5ff2
Matheus manuel <matheusmanuel111@gmail.com>
2023-10-19 09:46:02 +0100

    [backend] update - configurei o cors para receber requisições de outros sites😲

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mantraRoutes = require('./routes/mantraRoutes');
+const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = 4350;
 
 /*
     - Os middlewares do Express são usados para processar as solicitações antes que elas atinjam as rotas.
@@ -9,7 +10,7 @@ const port = 3000;
     localizados na pasta public. Ele mapeia esses arquivos para serem acessíveis em URLs.
     - express.json(): Este middleware permite que o aplicativo interprete solicitações no formato JSON.
 */
-
+app.use(cors());
 app.use(express.json());
 app.use(mantraRoutes);
 
