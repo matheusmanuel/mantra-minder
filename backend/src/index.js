60f961cfd04a5a378483a5f7783895bb2fcae184
Matheus manuel <matheusmanuel111@gmail.com>
2023-10-28 12:33:13 +0100

    [electron] - integrei com o electron.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,6 @@ const cors = require("cors");
 const app = express();
 const port = 4350;
 
-/*
-    - Os middlewares do Express são usados para processar as solicitações antes que elas atinjam as rotas.
-    - express.static: Este middleware é usado para servir arquivos estáticos, como imagens, CSS ou JavaScript, 
-    localizados na pasta public. Ele mapeia esses arquivos para serem acessíveis em URLs.
-    - express.json(): Este middleware permite que o aplicativo interprete solicitações no formato JSON.
-*/
 app.use(cors());
 app.use(express.json());
 app.use(mantraRoutes);
---
+++ frontend/electron/electron.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+const isDev = process.env.IS_DEV == "true" ? true : false;
+
+const apiMantraMinder = require('../../backend/src/index.js');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 650,
+    autoHideMenuBar: true,
+    resizable: false,
+    frame: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+ 
+  mainWindow.webContents.setWindowOpenHandler((edata) => {
+    shell.openExternal(edata.url);
+    return { action: "deny" };
+  });
+ 
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    //mainWindow.webContents.openDevTools();
+  }
+ 
+}
+ 
+ 
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+ 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
