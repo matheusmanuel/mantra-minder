d2d5b1042276eec35536d8803c6f4c1a1ddf415b
Matheus manuel <matheusmanuel111@gmail.com>
2023-10-28 13:04:42 +0100

    bug fixed: agora os eu já consigo abrir os link apartir de electron

--- frontend/electron/electron.js
+++ frontend/electron/electron.js
@@ -1,50 +1,46 @@
-const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const path = require("path");
+const { app, BrowserWindow, shell } = require("electron");
 const isDev = process.env.IS_DEV == "true" ? true : false;
 
-const apiMantraMinder = require('../../backend/src/index.js');
+const apiMantraMinder = require("../../backend/src/index.js");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1024,
     height: 650,
     autoHideMenuBar: true,
-    resizable: false,
     frame: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
-      contextIsolation: false
     },
   });
- 
-  mainWindow.webContents.setWindowOpenHandler((edata) => {
-    shell.openExternal(edata.url);
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
     return { action: "deny" };
   });
- 
+
   mainWindow.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../dist/index.html')}`
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../dist/index.html")}`
   );
   // Open the DevTools.
   if (isDev) {
-    //mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
   }
- 
 }
- 
- 
+
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
- 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
